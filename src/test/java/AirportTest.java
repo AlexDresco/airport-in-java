802df43aa8dab8607c2c21a53ee50754ff90a1c8
AlexDresco <alexandra.d1510@gmail.com>
2018-10-27 09:02:15 +0100

    Test passing - land, takeoff and weather

--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,37 +1,56 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.util.ArrayList;
-
-import static org.mockito.Mockito.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
 
 class AirportTest {
 
     @Mock
     Plane planeMock;
 
+    @Mock
+    Weather weather;
+
+    @InjectMocks
+    Airport airport;
+
+
     @BeforeEach
-    public void beforeEachTest(){
-        Weather weatherPlane = mock(Weather.class);
-        when(weatherPlane.isSunny()).thenReturn(true);
+    void beforeEachTest(){
+        airport = new Airport();
+        MockitoAnnotations.initMocks(this);
+
     }
 
     @Test
-    public void airportStoresLandedPlane(){
-        Airport airport = new Airport();
+    void airportStoresLandedPlane(){
+        when(weather.isSunny()).thenReturn(true);
         airport.land(planeMock);
         assertEquals(planeMock, airport.storage.get(0));
     }
 
     @Test
-    public void airportRecordsTakenOffPlane(){
-        Airport airport = new Airport();
+    void airportRecordsTakenOffPlane(){
+        when(weather.isSunny()).thenReturn(true);
         airport.land(planeMock);
         airport.takeOff(planeMock);
         assertTrue(airport.storage.isEmpty());
     }
 
+    @Test
+    void planeCantLandBadWeather(){
+        when(weather.isSunny()).thenReturn(false);
+        assertEquals("Plane can't land", airport.land(planeMock));
+    }
+
+    @Test
+    void planeCantTakeOffBadWeather(){
+        when(weather.isSunny()).thenReturn(false);
+        assertEquals("Plane can't take off", airport.takeOff(planeMock));
+    }
 }
